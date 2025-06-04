0b99e86cc3e17ed8004280ab728a4b331c9731ac
NoriDev <m1nthing2322@gmail.com>
2025-06-05 02:10:28 +0900

    test

--- Sources/VRCKit/Models/UnityPackage/UnityPackageModel.swift
+++ Sources/VRCKit/Models/UnityPackage/UnityPackageModel.swift
@@ -12,11 +12,7 @@ import MemberwiseInit
 public struct UnityPackage: Sendable, Identifiable, Hashable {
     public let id: String
     @InitWrapper(type: SafeDecoding<URL>)
-    public var assetUrl: URL? {
-        get { _assetUrl.wrappedValue }
-        set { _assetUrl.wrappedValue = newValue }
-    }
-    @SafeDecoding private var _assetUrl: URL?
+    public let assetUrl: SafeDecoding<URL>
     public let assetVersion: Int
     public let createdAt: OptionalISO8601Date
     public let platform: Platform
@@ -26,17 +22,36 @@ public struct UnityPackage: Sendable, Identifiable, Hashable {
     public enum Platform: String, Codable, Sendable {
         case android, ios, standalonewindows
     }
+
+    public var safeAssetUrl: URL? {
+        return assetUrl.wrappedValue
+    }
 }
 
 extension UnityPackage: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case id, assetUrl, assetVersion, createdAt, platform, unitySortNumber, unityVersion
+    }
+
     public init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(UnityPackage.ID.self, forKey: .id)
-        _assetUrl = try container.decode(SafeDecoding<URL>.self, forKey: .assetUrl)
+        id = try container.decode(String.self, forKey: .id)
+        assetUrl = try container.decode(SafeDecoding<URL>.self, forKey: .assetUrl)
         assetVersion = try container.decode(Int.self, forKey: .assetVersion)
         createdAt = try container.decodeIfPresent(OptionalISO8601Date.self, forKey: .createdAt) ?? OptionalISO8601Date()
         platform = try container.decode(UnityPackage.Platform.self, forKey: .platform)
         unitySortNumber = try container.decode(Int.self, forKey: .unitySortNumber)
         unityVersion = try container.decode(String.self, forKey: .unityVersion)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(assetUrl, forKey: .assetUrl)
+        try container.encode(assetVersion, forKey: .assetVersion)
+        try container.encode(createdAt, forKey: .createdAt)
+        try container.encode(platform, forKey: .platform)
+        try container.encode(unitySortNumber, forKey: .unitySortNumber)
+        try container.encode(unityVersion, forKey: .unityVersion)
     }
 }
